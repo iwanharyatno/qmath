a7944bfc93d744c8bb25c4cfb7fb0bfb744231ae
iwanharyatno <iwnharry61@gmail.com>
2021-11-28 20:46:38 +0700

    Added settings feature

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-console.log("hello");
+import { setting } from './settings';
+
+function main() {
+    setting.init();
+    const fieldNum1 = document.getElementById('num-1');
+    const fieldNum2 = document.getElementById('num-2');
+    const fieldOperator = document.getElementById('operator');
+    const fieldResult = document.getElementById('result');
+    
+    const buttonEvaluate = document.getElementById('evaluate');
+    const buttonRandom = document.getElementById('random');
+}
+
+function random(min, max) {
+    const value = min + (Math.random() * (max - min));
+    return Math.round(value);
+}
+
+window.onload = main;
---
+++ src/settings.js
@@ -0,0 +1,40 @@
+export const setting = {
+    init: function() {
+        this.isOpen = false;
+        this._triggerer = document.getElementById('setting-icon');
+        this._settings = document.getElementById('settings')
+        this._operation = document.getElementById('settings-operation');
+        this._num1Range = {
+            min: document.getElementById('num1-min'),
+            max: document.getElementById('num1-max')
+        };
+        this._num2Range = {
+            min: document.getElementById('num2-min'),
+            max: document.getElementById('num2-max')
+        };
+        this._buttonSave = document.getElementById('save-settings');
+
+
+        this._buttonSave.onclick = () => {
+            this.update();
+            this._settings.style.display = "none";
+        };
+        this._triggerer.onclick = () => {
+            if (this.isOpen) {
+                this.isOpen = false;
+                this._settings.style.display = "none";
+            } else {
+                this.isOpen = true;
+                this._settings.style.display = "initial";
+            }
+        }
+        this.update();
+    },
+    update: function() {
+        this.OPERATION = this._operation.value;
+        this.NUM1_MIN = this._num1Range.min.value;
+        this.NUM1_MAX = this._num1Range.max.value;
+        this.NUM2_MIN = this._num2Range.min.value;
+        this.NUM2_MAX = this._num2Range.max.value;
+    }
+}
